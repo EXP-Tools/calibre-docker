3a35ed15d6d050fc682f6c9e2cb27ea1e41eedc4
lyy289065406 <289065406@qq.com>
2020-10-13 12:46:35 +0800

    修正构建参数异常

--- README.md
+++ README.md
@@ -38,7 +38,7 @@
 - 宿主机安全组/防火墙（iptables/firewall）放行这些端口的入口流量： 80、8080、8081、8083
 - 下载仓库： `git clone https://github.com/lyy289065406/calibre-docker /usr/local/calibre-docker`
 - 打开仓库目录： `cd /usr/local/calibre-docker`
-- 构建镜像并运行： `caddy_user=admin caddy_pass=admin123 docker-compose up -d` （完全启动约需要 2 分钟）
+- 构建镜像并运行： `./run.sh -u admin -p admin123` （完全启动约需要 2 分钟）
 
 > 构建时设置的是 calibre 后端管理页面的账密，calibre-web 前端账密只能在 CMS 上修改。
 
---
+++ run.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# 运行 Calibre 服务
+#------------------------------------------------
+# 命令执行示例：
+# ./run.sh -u admin -p admin123
+#------------------------------------------------
+
+USERNAME="admin"
+PASSWORD="admin123"
+
+set -- `getopt u:p: "$@"`
+while [ -n "$1" ]
+do
+  case "$1" in
+    -u) USERNAME="$2"
+        shift ;;
+    -p) PASSWORD="$2"
+        shift ;;
+  esac
+  shift
+done
+
+PASSWORD=`echo -n ${PASSWORD} | openssl md5 | awk '{print $2}'`
+username=${USERNAME} pass_md5=${PASSWORD} docker-compose up -d
