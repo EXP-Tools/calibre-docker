42b273999d4d215f1e914bb3e952f4e364d15870
EXP <289065406@qq.com>
2021-08-24 06:42:55 +0000

    0824

--- run.sh
+++ run.sh
@@ -2,15 +2,15 @@
 # 运行 Calibre 服务
 #------------------------------------------------
 # 命令执行示例：
-# ./run.sh -u admin -p admin123 -u 1000 -g 1000
+# ./run.sh -u admin -p admin123 -i 1000 -g 1000
 #------------------------------------------------
 
 USERNAME="admin"
 PASSWORD="admin123"
-UID=`id | awk -F '[(=]' '{print $2}'`
-GID=`id | awk -F '[(=]' '{print $4}'`
+U_ID=`id | awk -F '[(=]' '{print $2}'`
+G_ID=`id | awk -F '[(=]' '{print $4}'`
 
-set -- `getopt u:p:u:g: "$@"`
+set -- `getopt u:p:i:g: "$@"`
 while [ -n "$1" ]
 do
   case "$1" in
@@ -18,13 +18,245 @@ do
         shift ;;
     -p) PASSWORD="$2"
         shift ;;
-    -u) UID="$2"
+    -i) U_ID="$2"
         shift ;;
-    -g) GID="$2"
+    -g) G_ID="$2"
         shift ;;
   esac
   shift
 done
 
 PASS_MD5=`echo -n ${PASSWORD} | openssl md5 | awk '{print $2}'`
-username=${USERNAME} pass_md5=${PASS_MD5} uid=${UID} gid=${GID} docker-compose up -d
+username=${USERNAME} pass_md5=${PASS_MD5} uid=${U_ID} gid=${G_ID} docker-compose up -d#!/bin/bash
+# 运行 Calibre 服务
+#------------------------------------------------
+# 命令执行示例：
+# ./run.sh -u admin -p admin123 -i 1000 -g 1000
+#------------------------------------------------
+
+USERNAME="admin"
+PASSWORD="admin123"
+U_ID=`id | awk -F '[(=]' '{print $2}'`
+G_ID=`id | awk -F '[(=]' '{print $4}'`
+
+set -- `getopt u:p:i:g: "$@"`
+while [ -n "$1" ]
+do
+  case "$1" in
+    -u) USERNAME="$2"
+        shift ;;
+    -p) PASSWORD="$2"
+        shift ;;
+    -i) U_ID="$2"
+        shift ;;
+    -g) G_ID="$2"
+        shift ;;
+  esac
+  shift
+done
+
+PASS_MD5=`echo -n ${PASSWORD} | openssl md5 | awk '{print $2}'`
+username=${USERNAME} pass_md5=${PASS_MD5} uid=${U_ID} gid=${G_ID} docker-compose up -d#!/bin/bash
+# 运行 Calibre 服务
+#------------------------------------------------
+# 命令执行示例：
+# ./run.sh -u admin -p admin123 -i 1000 -g 1000
+#------------------------------------------------
+
+USERNAME="admin"
+PASSWORD="admin123"
+U_ID=`id | awk -F '[(=]' '{print $2}'`
+G_ID=`id | awk -F '[(=]' '{print $4}'`
+
+set -- `getopt u:p:i:g: "$@"`
+while [ -n "$1" ]
+do
+  case "$1" in
+    -u) USERNAME="$2"
+        shift ;;
+    -p) PASSWORD="$2"
+        shift ;;
+    -i) U_ID="$2"
+        shift ;;
+    -g) G_ID="$2"
+        shift ;;
+  esac
+  shift
+done
+
+PASS_MD5=`echo -n ${PASSWORD} | openssl md5 | awk '{print $2}'`
+username=${USERNAME} pass_md5=${PASS_MD5} uid=${U_ID} gid=${G_ID} docker-compose up -d#!/bin/bash
+# 运行 Calibre 服务
+#------------------------------------------------
+# 命令执行示例：
+# ./run.sh -u admin -p admin123 -i 1000 -g 1000
+#------------------------------------------------
+
+USERNAME="admin"
+PASSWORD="admin123"
+U_ID=`id | awk -F '[(=]' '{print $2}'`
+G_ID=`id | awk -F '[(=]' '{print $4}'`
+
+set -- `getopt u:p:i:g: "$@"`
+while [ -n "$1" ]
+do
+  case "$1" in
+    -u) USERNAME="$2"
+        shift ;;
+    -p) PASSWORD="$2"
+        shift ;;
+    -i) U_ID="$2"
+        shift ;;
+    -g) G_ID="$2"
+        shift ;;
+  esac
+  shift
+done
+
+PASS_MD5=`echo -n ${PASSWORD} | openssl md5 | awk '{print $2}'`
+username=${USERNAME} pass_md5=${PASS_MD5} uid=${U_ID} gid=${G_ID} docker-compose up -d#!/bin/bash
+# 运行 Calibre 服务
+#------------------------------------------------
+# 命令执行示例：
+# ./run.sh -u admin -p admin123 -i 1000 -g 1000
+#------------------------------------------------
+
+USERNAME="admin"
+PASSWORD="admin123"
+U_ID=`id | awk -F '[(=]' '{print $2}'`
+G_ID=`id | awk -F '[(=]' '{print $4}'`
+
+set -- `getopt u:p:i:g: "$@"`
+while [ -n "$1" ]
+do
+  case "$1" in
+    -u) USERNAME="$2"
+        shift ;;
+    -p) PASSWORD="$2"
+        shift ;;
+    -i) U_ID="$2"
+        shift ;;
+    -g) G_ID="$2"
+        shift ;;
+  esac
+  shift
+done
+
+PASS_MD5=`echo -n ${PASSWORD} | openssl md5 | awk '{print $2}'`
+username=${USERNAME} pass_md5=${PASS_MD5} uid=${U_ID} gid=${G_ID} docker-compose up -d#!/bin/bash
+# 运行 Calibre 服务
+#------------------------------------------------
+# 命令执行示例：
+# ./run.sh -u admin -p admin123 -i 1000 -g 1000
+#------------------------------------------------
+
+USERNAME="admin"
+PASSWORD="admin123"
+U_ID=`id | awk -F '[(=]' '{print $2}'`
+G_ID=`id | awk -F '[(=]' '{print $4}'`
+
+set -- `getopt u:p:i:g: "$@"`
+while [ -n "$1" ]
+do
+  case "$1" in
+    -u) USERNAME="$2"
+        shift ;;
+    -p) PASSWORD="$2"
+        shift ;;
+    -i) U_ID="$2"
+        shift ;;
+    -g) G_ID="$2"
+        shift ;;
+  esac
+  shift
+done
+
+PASS_MD5=`echo -n ${PASSWORD} | openssl md5 | awk '{print $2}'`
+username=${USERNAME} pass_md5=${PASS_MD5} uid=${U_ID} gid=${G_ID} docker-compose up -d#!/bin/bash
+# 运行 Calibre 服务
+#------------------------------------------------
+# 命令执行示例：
+# ./run.sh -u admin -p admin123 -i 1000 -g 1000
+#------------------------------------------------
+
+USERNAME="admin"
+PASSWORD="admin123"
+U_ID=`id | awk -F '[(=]' '{print $2}'`
+G_ID=`id | awk -F '[(=]' '{print $4}'`
+
+set -- `getopt u:p:i:g: "$@"`
+while [ -n "$1" ]
+do
+  case "$1" in
+    -u) USERNAME="$2"
+        shift ;;
+    -p) PASSWORD="$2"
+        shift ;;
+    -i) U_ID="$2"
+        shift ;;
+    -g) G_ID="$2"
+        shift ;;
+  esac
+  shift
+done
+
+PASS_MD5=`echo -n ${PASSWORD} | openssl md5 | awk '{print $2}'`
+username=${USERNAME} pass_md5=${PASS_MD5} uid=${U_ID} gid=${G_ID} docker-compose up -d#!/bin/bash
+# 运行 Calibre 服务
+#------------------------------------------------
+# 命令执行示例：
+# ./run.sh -u admin -p admin123 -i 1000 -g 1000
+#------------------------------------------------
+
+USERNAME="admin"
+PASSWORD="admin123"
+U_ID=`id | awk -F '[(=]' '{print $2}'`
+G_ID=`id | awk -F '[(=]' '{print $4}'`
+
+set -- `getopt u:p:i:g: "$@"`
+while [ -n "$1" ]
+do
+  case "$1" in
+    -u) USERNAME="$2"
+        shift ;;
+    -p) PASSWORD="$2"
+        shift ;;
+    -i) U_ID="$2"
+        shift ;;
+    -g) G_ID="$2"
+        shift ;;
+  esac
+  shift
+done
+
+PASS_MD5=`echo -n ${PASSWORD} | openssl md5 | awk '{print $2}'`
+username=${USERNAME} pass_md5=${PASS_MD5} uid=${U_ID} gid=${G_ID} docker-compose up -d#!/bin/bash
+# 运行 Calibre 服务
+#------------------------------------------------
+# 命令执行示例：
+# ./run.sh -u admin -p admin123 -i 1000 -g 1000
+#------------------------------------------------
+
+USERNAME="admin"
+PASSWORD="admin123"
+U_ID=`id | awk -F '[(=]' '{print $2}'`
+G_ID=`id | awk -F '[(=]' '{print $4}'`
+
+set -- `getopt u:p:i:g: "$@"`
+while [ -n "$1" ]
+do
+  case "$1" in
+    -u) USERNAME="$2"
+        shift ;;
+    -p) PASSWORD="$2"
+        shift ;;
+    -i) U_ID="$2"
+        shift ;;
+    -g) G_ID="$2"
+        shift ;;
+  esac
+  shift
+done
+
+PASS_MD5=`echo -n ${PASSWORD} | openssl md5 | awk '{print $2}'`
+username=${USERNAME} pass_md5=${PASS_MD5} uid=${U_ID} gid=${G_ID} docker-compose up -d
